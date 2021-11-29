b7149db1632adffac37dca6a8a55dc91aae3edee
Imaad <53949355+ImaadIrshad@users.noreply.github.com>
2021-11-29 23:49:48 +0400

    Update

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,7 +1,22 @@
 // deploy code will go here
-const HDWalletProvider = require('@truffle/hdwallet-provider1;');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const {interface, bytecode} = require('./compile');
 
 const provider = new HDWalletProvider(
-	'')
+	'avoid episode next action segment shell rival fresh kidney arch equal toward',
+	'https://rinkeby.infura.io/v3/d50b348148e6481aa1313f56c61a9e3c'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+	const accounts = await web3.eth.getAccounts();
+	console.log('Attempting to deploy from account ', accounts[0]);
+	const result = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data : bytecode, arguments: ['Hi there!']})
+		.send({ gas : '1000000', from : accounts[0]})
+
+	console.log('Contract deployed to', result.options.address);
+	provider.engine.stop()
+};
+deploy();
